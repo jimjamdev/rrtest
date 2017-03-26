83b997d4968379b63d102a5fbf5d2faf0d30c748
ian jamieson <cogocreative@gmail.com>
2017-03-27 00:07:18 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
                 loader: 'babel-loader'
             },
             {
-                test: /\.(tsx)$/,
+                test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 loader: 'awesome-typescript-loader'
             }
