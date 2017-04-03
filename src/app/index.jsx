8f9fc623f08b466164d60ac98eec7e4f00738606
Ian Jamieson <ian.jamieson@mi-c3.com>
2017-04-03 09:57:35 +0200

    fixed old dependencies in index

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
-import * as injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 
 import store from './store';
@@ -14,13 +11,10 @@ import HomeContainer from './containers/home/home.container';
 import BlogContainer from './containers/blog/blog.container';
 
 ReactDOM.render((
-        <MuiThemeProvider>
             <Provider store={store}>
                 <BrowserRouter>
                     <Route path="/" component={AppContainer} />
                 </BrowserRouter>
             </Provider>
-        </MuiThemeProvider>
-
     ),
     document.getElementById('root'));
