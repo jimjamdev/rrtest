56e3555d86dc6d3bf95e45e49e617a0a0f6abd89
Ian Jamieson <ian.jamieson@mi-c3.com>
2017-05-16 09:54:57 +0200

    blog

--- server/src/models/blog.model.js
+++ server/src/models/blog.model.js
@@ -1,10 +1,11 @@
 // blog-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const blog = new mongooseClient.Schema({
+    title: { type: String, required: true },
     text: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
